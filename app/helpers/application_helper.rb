d1196a00bff722a6b2458608ff11541a38155f29
Anton Dyachuk <anton.dyachuk@gmail.com>
2025-01-19 10:43:07 +0300

    Render menu with helper and mark item active depends on path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def render_menu_items
+    safe_join(menu_items.map do |item|
+      content_tag :li, class: 'nav-item' do
+        css_class = 'nav-link'
+        css_class << ' active' if request.path == item[:path]
+        link_to item[:title], item[:path], class: css_class
+      end
+    end)
+  end
+
+  private
+
+  def menu_items
+    [
+      {title: 'Dashboard', path: dashboard_path},
+      {title: 'Posts', path: posts_path},
+    ]
+  end
 end
